02c3ff123ccc485b9c9640df2c8c716e57a0fe8d
maxefimov <efimov.top.academy@gmail.com>
2023-03-14 20:18:43 +0300

    14-03-23

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcTagHelpersApp.Models;
+using StepClasses;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MvcTagHelpersApp.Controllers
 {
@@ -8,9 +10,26 @@ namespace MvcTagHelpersApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        List<Country> countries;
+        List<Company> companies;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
+            countries = new List<Country>()
+            {
+                new Country("Russia", "Moscow"),
+                new("USA", "Washington"),
+            };
+
+            companies = new List<Company>()
+            {
+                new("Yandex", countries[0]),
+                new("Ozon", countries[0]),
+                new("Google", countries[1]),
+                new("Amazone", countries[1]),
+                new("Mail Group", countries[0]),
+            };
         }
 
         public IActionResult Index()
@@ -26,6 +45,20 @@ namespace MvcTagHelpersApp.Controllers
         public IActionResult Work()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Empl()
+        {
+            ViewBag.Companies = new SelectList(companies, "Title", "Title");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Empl(Employe employe)
+        {
+            Company? company = companies.FirstOrDefault(c => c == employe?.Company);
+            return Content($"Create new Emplye: name {employe.Name}, company {employe.Company?.Title}");
         }
 
         public IActionResult Item(string title, int id)
